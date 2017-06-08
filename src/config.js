5da3f90cd9392f61b4952d33da1490a61897b3a9
Harrison Dews <harrisondews@gmail.com>
2017-06-08 18:52:47 -0400

    updated config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 var config1 = require('config1');
 var config2 = require('config2');
+var config3 = require('config3');
 
 var level_to_config_generator = {
     0: {},
     1: config1,
     2: config2,
-    3: config2,
-    4: config2,
-    5: config2,
-    6: config2,
-    7: config2,
-    8: config2
+    3: config3,
+    4: config3,
+    5: config3,
+    6: config3,
+    7: config3,
+    8: config3
 };
 
 module.exports = level_to_config_generator;
